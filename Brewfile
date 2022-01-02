cfa8429f9dae4cfd649a6bad624faa133d8bd438
Benjamin Dos Santos <dosbenjamin@icloud.com>
2022-01-02 21:18:44 +0100

    Add brewfile

---
+++ Brewfile
@@ -0,0 +1,34 @@
+brew 'coreutils'
+brew 'wget'
+
+brew 'openssh'
+brew 'vim'
+brew 'nvm'
+brew 'composer'
+brew 'git'
+brew 'php'
+brew 'pnpm', args: ['ignore-dependencies']
+brew 'yarn', args: ['ignore-dependencies']
+
+tap 'homebrew/cask-fonts'
+cask 'font-jetbrains-mono'
+
+tap 'homebrew/cask-drivers'
+cask 'logitech-options'
+
+cask 'appcleaner'
+cask 'chromium'
+cask 'discord'
+cask 'figma'
+cask 'firefox'
+cask 'keka'
+cask 'kekaexternalhelper'
+cask 'messenger'
+cask 'onyx'
+cask 'qbittorrent'
+cask 'rectangle'
+cask 'slack'
+cask 'spotify'
+cask 'visual-studio-code'
+cask 'vlc'
+cask 'whatsapp'
--- install.sh
+++ install.sh
@@ -12,47 +12,8 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-
-# Install `wget`.
-brew install wget
-
-# Install more recent versions of some macOS tools.
-brew install openssh
-brew install vim
-
-# Install other useful binaries.
-brew install nvm
-brew install composer
-brew install git
-brew install php
-brew install pnpm --ignore-dependencies
-
-# Install JetBrains mono font.
-brew tap homebrew/cask-fonts
-brew install font-jetbrains-mono --cask
-
-# Install drivers.
-brew tap homebrew/cask-drivers
-brew install logitech-options --cask
-
-# Install other useful softwares.
-brew install appcleaner --cask
-brew install chromium --cask
-brew install discord --cask
-brew install figma --cask
-brew install keka --cask
-brew install kekaexternalhelper --cask
-brew install messenger --cask
-brew install onyx --cask
-brew install qbittorrent --cask
-brew install rectangle --cask
-brew install slack --cask
-brew install spotify --cask
-brew install visual-studio-code --cask
-brew install whatsapp --cask
+# Install binaries and softwares.
+brew bundle
 
 # Remove outdated versions from the cellar.
 brew cleanup
